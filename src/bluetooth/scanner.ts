108c27a2b1fd9b2f565516d8e5b48c9443f2c9ff
Stephen Augenstein <stephen@chromedshark.com>
2025-12-29 13:15:25 -0500

    Set up basic project

---
+++ src/bluetooth/scanner.ts
@@ -0,0 +1,24 @@
+/**
+ * Bluetti Web Bluetooth Scanner
+ *
+ * This module provides functions for discovering Bluetti devices
+ * using the Web Bluetooth API.
+ */
+
+/** The GATT service UUID used by all Bluetti devices */
+export const BLUETTI_SERVICE_UUID = 0xff00;
+
+/**
+ * Request a Bluetti device from the user via the Web Bluetooth API.
+ *
+ * This triggers the browser's device picker dialog filtered to show
+ * only devices advertising the Bluetti service UUID (0xFF00).
+ *
+ * @returns The selected BluetoothDevice
+ * @throws DOMException if the user cancels or no device is found
+ */
+export async function requestBluettiDevice(): Promise<BluetoothDevice> {
+  return navigator.bluetooth.requestDevice({
+    filters: [{ services: [BLUETTI_SERVICE_UUID] }],
+  });
+}
